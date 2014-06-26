0dd670b5b53968a213ceeac2a79e2812af375cf1
Notebook <Notebook@Notebook-PC>
2014-06-26 22:54:47 +0300

    Best times menu working under android.

--- src/eu/veldsoft/four/row/solitaire/MenuActivity.java
+++ src/eu/veldsoft/four/row/solitaire/MenuActivity.java
@@ -39,6 +39,15 @@ public class MenuActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_menu);
 
+		((Button) findViewById(R.id.statistics_button))
+				.setOnClickListener(new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						MenuActivity.this.startActivity(new Intent(
+								MenuActivity.this, StatisticsActivity.class));
+					}
+				});
+
 		((Button) findViewById(R.id.best_times_button))
 				.setOnClickListener(new OnClickListener() {
 					@Override
