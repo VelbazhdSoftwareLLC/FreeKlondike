369a526d3054d29e7258aa8a00009692802d9784
InaBaltadzhieva <i.baltadzhiev@gmail.com>
2014-07-15 18:24:51 +0300

    Statistics split in tabs.

---
+++ src/eu/veldsoft/four/row/solitaire/StatisticsTab4Activity.java
@@ -0,0 +1,35 @@
+package eu.veldsoft.four.row.solitaire;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class StatisticsTab4Activity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_statistics_tab4);
+
+		((Button) findViewById(R.id.ok_statistics))
+				.setOnClickListener(new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						finish();
+					}
+				});
+		((Button) findViewById(R.id.reset_statistics))
+				.setOnClickListener(new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						Toast.makeText(getApplicationContext(), "Reset 5!",
+								Toast.LENGTH_SHORT).show();
+					}
+				});
+	}
+}
